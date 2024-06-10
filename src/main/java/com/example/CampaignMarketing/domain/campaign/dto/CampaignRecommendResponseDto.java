a4c93f34e3c2739955e48525655a0ccdf55fafa1
sehwan24 <1224ssanta@naver.com>
2024-06-10 14:37:33 +0900

    Feat : Python connect

---
+++ src/main/java/com/example/CampaignMarketing/domain/campaign/dto/CampaignRecommendResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.CampaignMarketing.domain.campaign.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CampaignRecommendResponseDto {
+
+    private String name;
+}
