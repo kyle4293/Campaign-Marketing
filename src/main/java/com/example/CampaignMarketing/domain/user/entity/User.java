25871eb4c0f0540b0c067a78d8105888058497b9
최창규 <chang4293@gmail.com>
2024-06-21 18:13:01 +0900

    Fix: FetchType.EAGER

--- src/main/java/com/example/CampaignMarketing/domain/user/entity/User.java
+++ src/main/java/com/example/CampaignMarketing/domain/user/entity/User.java
@@ -38,17 +38,17 @@ public class User extends Timestamped {
     private String bio; //자기소개
     private String job;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "considerations", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "consideration")
     private List<String> considerations;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "fav_foods", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "fav_food")
     private List<String> fav_foods;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "cant_foods", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "cant_food")
     private List<String> cant_foods;
